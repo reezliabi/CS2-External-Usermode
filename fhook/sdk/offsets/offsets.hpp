8dd02220a5aec036c9cb3604111400e17901e8e2
Reezli <116123222+reezliabi@users.noreply.github.com>
2024-05-02 00:32:58 +0200

    Update offsets.hpp
    Updated to last game version

--- fhook/sdk/offsets/offsets.hpp
+++ fhook/sdk/offsets/offsets.hpp
@@ -128,24 +128,24 @@ namespace unknowncheats {
 	}
 
 	namespace dragged_offsets {
-		inline DWORD entity_list;
-		inline DWORD matrix;
-		inline DWORD view_angle;
-		inline DWORD local_player_controller;
-		inline DWORD local_player_pawn;
-		inline DWORD force_jump;
-		inline DWORD global_vars;
+		inline DWORD entity_list = 0x17C1950;
+		inline DWORD matrix = 0x1820150;
+		inline DWORD view_angle = 0x1880DC0;
+		inline DWORD local_player_controller = 0x1810F48;
+		inline DWORD local_player_pawn = 0x16C8F58;
+		inline DWORD force_jump = 0x16C23A0;
+		inline DWORD global_vars = 0x16BDCB8;
 		inline DWORD inventory_services;
-		inline DWORD planted_c4;
+		inline DWORD planted_c4 = 0x1824A88;
 	}
 
 	namespace signatures {
 		const std::string inventory_services = "E8 ?? ?? ?? ?? 8B 45 D0 48 8B 55 D8"; // outdate
 		const std::string global_vars = "48 89 0D ?? ?? ?? ?? 48 89 41"; // old?
 		const std::string entity_list = "48 8B 0D ?? ?? ?? ?? 48 89 7C 24 ?? 8B FA C1 EB";
-		const std::string local_player_controller = "48 8B 05 ?? ?? ?? ?? 48 85 C0 74 4F";
+		const std::string local_player_controller = "48 8B 05 ?? ?? ?? ?? 48 85 C0 74 ?? 8B 88";
 		const std::string view_angles = "48 8B 0D ?? ?? ?? ?? E9 ?? ?? ?? ?? CC CC CC CC 40 55";
-		const std::string view_matrix = "48 8D 0D ?? ?? ?? ?? 48 C1 E0 06";
+		const std::string view_matrix = "48 8D 05 ?? ?? ?? ?? 48 8B D3 4C 8D 05";
 		const std::string local_player_pawn = "48 8D 05 ?? ?? ?? ?? C3 CC CC CC CC CC CC CC CC 48 83 EC ?? 8B 0D";
 		const std::string force_jump = "48 8B 05 ?? ?? ?? ?? 48 8D 1D ?? ?? ?? ?? 48 89 45";
 		const std::string planted_c4 = "48 8B 15 ?? ?? ?? ?? FF C0 48 8D 4C 24 40";
